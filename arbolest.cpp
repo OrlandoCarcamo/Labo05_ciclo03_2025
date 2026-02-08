0a7f872d4786b73dde1f4ffcb92ec1368a31ff2a
davidcasstro <00369425@uca.edu.sv>
2026-02-07 22:50:05 -0600

    Funcion mostrar por orden de carnet implementada

---
+++ arbolest.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+struct Estudiante {
+ int carnet;
+ char nombre[50];
+ float nota;
+ Estudiante* izquierdo;
+ Estudiante* derecho;
+};
+
+Estudiante* crearEstudiante(int carnet, char nom[], float nota) {
+ Estudiante* nuevo = new Estudiante();
+ nuevo->carnet = carnet;
+ nuevo->nota = nota;
+ nuevo->izquierdo = NULL;
+ nuevo->derecho = NULL;
+
+ // Copiar el nombre
+ int i = 0;
+ while(nom[i] != '\0' && i < 49) {
+
+
+ nuevo->nombre[i] = nom[i];
+ i++;
+ }
+ nuevo->nombre[i] = '\0';
+
+ return nuevo;
+}
+
+Estudiante* insertar(Estudiante* raiz, int carnet, char nombre[], float nota) {
+
+    if(raiz == NULL)
+        return crearEstudiante(carnet, nombre, nota);
+
+    if(carnet < raiz->carnet)
+        raiz->izquierdo = insertar(raiz->izquierdo, carnet, nombre, nota);
+
+    else if(carnet > raiz->carnet)
+        raiz->derecho = insertar(raiz->derecho, carnet, nombre, nota);
+
+    return raiz;
+}
+
+void mostrarEstudiantes(Estudiante* raiz) {
+    if (raiz != NULL) {
+        mostrarEstudiantes(raiz->izquierdo);
+        cout << "Carnet: " << raiz->carnet 
+             << " | Nombre: " << raiz->nombre 
+             << " | Nota: " << raiz->nota << endl;
+        mostrarEstudiantes(raiz->derecho);
+    }
+}
